d8a739b3b2663a2aeb79e8742db9a43c61f2e0ba
Zombox <ghostzb@outlook.com>
2025-08-25 00:10:41 +0700

    add: add global RoutersGroup

--- main.go
+++ main.go
@@ -1,26 +1,44 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/zombox0633/printer_backend_go/src"
+	"github.com/zombox0633/printer_backend_go/src/config"
 )
 
 func main() {
-	port := "8080"
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Panicln("Warning: .env file not found 🙀")
+	}
+
+	env := config.LoadConfig()
+	if env.Environment != "development" {
+		gin.SetMode(gin.ReleaseMode)
+	}
 
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{fmt.Sprintf("http://localhost:%v", 3000)},
+		AllowOrigins: []string{
+			"http://localhost:5173",
+			"http://localhost:3000",
+		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 	}))
 
-	log.Printf("Server starting on port http://localhost:%s 🎉", port)
-	router.Run(": " + port)
+	src.RoutersGroup(router)
+
+	log.Printf("Server starting on http://localhost:%s 🎉", env.Port)
+
+	if err := router.Run(":" + env.Port); err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
 }
